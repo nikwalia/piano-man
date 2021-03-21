135545d5c1756c5c9b854df110ac83df4801c938
Ben Schneider <bhs5@illinois.edu>
2021-03-21 16:38:35 -0500

    initial commit

---
+++ piano.py
@@ -0,0 +1,99 @@
+from klampt import WorldModel,RobotModel,RobotModelLink,Geometry3D
+from klampt.math import vectorops,so3,se3
+from klampt.model import ik
+from klampt import vis
+import math
+import numpy as np
+
+from klampt.model.create import primitives
+
+#I seem to have reached an impasse; that importing the robot model into klampt requires it to be compiled without edits,
+#meaning it has to successfully load and I cannot modify it, and unfortunately enough the Shadowhand has empty triangles
+#(2 sides are the same point) so it won't load the model, let alone whether or not it will work once fixed. Guess I'll ask Kris
+#this week?
+
+
+#you will need to change this to the absolute or relative path to Klampt-examples
+KLAMPT_EXAMPLES = '../cs498ir_s2021'
+
+def make_shelf(world,width,depth,height,wall_thickness=0.005):
+    """Makes a new axis-aligned "shelf" centered at the origin with
+    dimensions width x depth x height. Walls have thickness wall_thickness.
+    """
+    left = Geometry3D()
+    right = Geometry3D()
+    back = Geometry3D()
+    bottom = Geometry3D()
+    top = Geometry3D()
+    #method 1
+    left.loadFile(KLAMPT_EXAMPLES+"/data/objects/cube.off")
+    left.transform([wall_thickness,0,0,0,depth,0,0,0,height],[-width*0.5,-depth*0.5,0])
+    right.loadFile(KLAMPT_EXAMPLES+"/data/objects/cube.off")
+    right.transform([wall_thickness,0,0,0,depth,0,0,0,height],[width*0.5,-depth*0.5,0])
+    #method 2
+    back.loadFile(KLAMPT_EXAMPLES+"/data/objects/cube.off")
+    back.scale(width,wall_thickness,height)
+    back.translate([-width*0.5,depth*0.5,0])
+    #equivalent to back.transform([width,0,0,0,wall_thickness,0,0,0,height],[-width*0.5,depth*0.5,0])
+    #method 3
+    bottom = primitives.box(width,depth,wall_thickness,center=[0,0,0])
+    top = primitives.box(width,depth,wall_thickness,center=[0,0,height-wall_thickness*0.5])
+    shelfgeom = Geometry3D()
+    shelfgeom.setGroup()
+    for i,elem in enumerate([left,right,back,bottom,top]):
+        g = Geometry3D(elem)
+        shelfgeom.setElement(i,g)
+    shelf = world.makeTerrain("shelf")
+    shelf.geometry().set(shelfgeom)
+    shelf.appearance().setColor(0.2,0.6,0.3,1.0)
+    return shelf
+
+def create_terrain(world, width, length):
+    geo = primitives.box(width,length,0.05,center=[0,0,0])
+    terrain = world.makeTerrain("ground")
+    terrain.geometry().set(geo)
+    return terrain
+
+def show_env():
+    print("something")
+    world = WorldModel()
+    #file="sr_common-melodic-devel/sr_description/mujoco_models/urdfs/shadowhand_motor.urdf"
+    robot = RobotModel()
+    robot = robot.loadFile("sr_common-melodic-devel/sr_description/mujoco_models/urdfs/shadowhand_motor.urdf")
+    status = world.readFile("world.xml")
+    if not status:
+        print("file not read")
+        exit(-1)
+    vis.add("world", world)
+    #shelf = make_shelf(world, 0.5, 0.5, 0.5)
+    terrain = create_terrain(world, 5, 5)
+    vis.run()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    show_env()
+    
+    
---
+++ sr_common-melodic-devel/README.md
@@ -0,0 +1,13 @@
+# CI Statuses
+
+Check | Status
+---|---
+Documentation|[![Documentation Status](https://readthedocs.org/projects/shadow-robots-common-packages/badge/?version=latest)](http://shadow-robots-common-packages.readthedocs.org/)
+Build|[<img src="https://codebuild.eu-west-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiVTgvUEdhQXVvY1FZaklWbXVjL3VGV0FpMlNVdWVpdlo1MnlGTFh3SWhYa2FHNG85QjJmb2E3NEZpNGxVNmhMQmxDRE4xUk1GZnJaTm5RWDFpRHgyWHdRPSIsIml2UGFyYW1ldGVyU3BlYyI6IkxsQU1MVHZUUFJDRWNGSHAiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=melodic-devel"/>](https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/projects/auto_sr_common_melodic-devel_install_check/)
+Style|[<img src="https://codebuild.eu-west-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoia1BNdEllZ1ZBdmdYUzJEZmQwYWF6Nk9RR0IyNU8yMDZ4MjIvcGxKUUtoSWhKekZmVWJYL1ArVUY1UG9rZ09YaFNEZ0hzdDVCWGpyRldHWjNSTUtMZzZvPSIsIml2UGFyYW1ldGVyU3BlYyI6Im5YUkYzeWhaYVRCc1ZyUk0iLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=melodic-devel"/>](https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/projects/auto_sr_common_melodic-devel_style_check/)
+Code Coverage|[<img src="https://codebuild.eu-west-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoidno4aGUrVFIvZ1pieE5wUHk4QXljUWV5YVU2RXpwWTdvOVczUFBwcXZLNk9rbzZYNFl0TEJRVm1IYUx3SXoxTE1oVDlYQ0FCSm84ZU82ZkhZbzFPUk40PSIsIml2UGFyYW1ldGVyU3BlYyI6InI3aDBGMGdKQ3lHRDdTZVkiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=melodic-devel"/>](https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/projects/auto_sr_common_melodic-devel_code_coverage/)
+
+
+
+# Shadow Robot Common packages
+This repository contains the bare minimum for communicating with the Shadow Hand from a remote computer: urdf models and messages.
