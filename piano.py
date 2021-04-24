69607cf6f117d1ada681dd63fa7ee6b2830bc3c0
Ben Schneider <bhs5@illinois.edu>
2021-04-23 20:18:56 -0500

    Early steps for IK and chord conversion

--- piano.py
+++ piano.py
@@ -7,47 +7,11 @@ import numpy as np
 
 from klampt.model.create import primitives
 
-#I seem to have reached an impasse; that importing the robot model into klampt requires it to be compiled without edits,
-#meaning it has to successfully load and I cannot modify it, and unfortunately enough the Shadowhand has empty triangles
-#(2 sides are the same point) so it won't load the model, let alone whether or not it will work once fixed. Guess I'll ask Kris
-#this week?
-
 
 #you will need to change this to the absolute or relative path to Klampt-examples
-KLAMPT_EXAMPLES = '../cs498ir_s2021'
-#KLAMPT_EXAMPLES = '../../../cs498ir_s2021'
+#KLAMPT_EXAMPLES = '../cs498ir_s2021'
+KLAMPT_EXAMPLES = '../../../cs498ir_s2021'
 
-# def make_shelf(world,width,depth,height,wall_thickness=0.005):
-#     """Makes a new axis-aligned "shelf" centered at the origin with
-#     dimensions width x depth x height. Walls have thickness wall_thickness.
-#     """
-#     left = Geometry3D()
-#     right = Geometry3D()
-#     back = Geometry3D()
-#     bottom = Geometry3D()
-#     top = Geometry3D()
-#     #method 1
-#     left.loadFile(KLAMPT_EXAMPLES+"/data/objects/cube.off")
-#     left.transform([wall_thickness,0,0,0,depth,0,0,0,height],[-width*0.5,-depth*0.5,0])
-#     right.loadFile(KLAMPT_EXAMPLES+"/data/objects/cube.off")
-#     right.transform([wall_thickness,0,0,0,depth,0,0,0,height],[width*0.5,-depth*0.5,0])
-#     #method 2
-#     back.loadFile(KLAMPT_EXAMPLES+"/data/objects/cube.off")
-#     back.scale(width,wall_thickness,height)
-#     back.translate([-width*0.5,depth*0.5,0])
-#     #equivalent to back.transform([width,0,0,0,wall_thickness,0,0,0,height],[-width*0.5,depth*0.5,0])
-#     #method 3
-#     bottom = primitives.box(width,depth,wall_thickness,center=[0,0,0])
-#     top = primitives.box(width,depth,wall_thickness,center=[0,0,height-wall_thickness*0.5])
-#     shelfgeom = Geometry3D()
-#     shelfgeom.setGroup()
-#     for i,elem in enumerate([left,right,back,bottom,top]):
-#         g = Geometry3D(elem)
-#         shelfgeom.setElement(i,g)
-#     shelf = world.makeTerrain("shelf")
-#     shelf.geometry().set(shelfgeom)
-#     shelf.appearance().setColor(0.2,0.6,0.3,1.0)
-#     return shelf
 
 #52 white keys, 36 black keys
 #Base scale is width of piano key = 0.1 at scale 1.0
@@ -55,14 +19,16 @@ def make_piano(world,scale):
     key_width = 0.11 * scale
     key_height = 0.1 * scale
     black_key_width = 0.05 * scale
+    key_length = 0.6 * scale
+    black_key_length = 0.6 * 0.6 * scale
     keys = []
     black_keys = []
     for i in range(52):
         key = make_key(scale)
-        key.translate([key_width * i,0,0.5 * scale])
+        key.translate([key_width * i,-1 * key_length,0.5 * scale])
         black_key = make_black_key(scale, i)
         if black_key:
-            black_key.translate([key_width * i - 1/2 * key_width + 1/2 * black_key_width,0,(key_height + 0.55) * scale])
+            black_key.translate([key_width * i - 1/2 * key_width + 1/2 * black_key_width,-1 * black_key_length,(key_height + 0.55) * scale])
             black_keys.append(black_key)
         keys.append(key)
         
@@ -91,7 +57,7 @@ def make_key(scale):
     return key
 
 def make_black_key(scale, index):
-    if index % 7 == 0 or index % 7 == 3:
+    if index % 7 == 2 or index % 7 == 5 or index == 0 or index == 51:
         return None
     key_width = 0.05 * scale
     key_height = 0.05 * scale
@@ -120,21 +86,101 @@ def show_env():
     vis.add("world", world)
     #shelf = make_shelf(world, 0.5, 0.5, 0.5)
     piano_scale = 0.25
-    # keys, black_keys = make_piano(world, piano_scale)
-    # vis.add("white_keys", keys)
-    # vis.add("black_keys", black_keys)
-    # cam = vis.camera.free()
+    keys, black_keys = make_piano(world, piano_scale)
+    vis.add("white_keys", keys)
+    vis.add("black_keys", black_keys)
+    cam = vis.camera.free()
+    locations = chordToKeys(world, "A", keys, black_keys)
+    location = locations[0]
+    vis.add("key",(location[0], location[1], location[2]))
+#     link_names = ['ra_base_link', 'ra_shoulder_link', 'ra_upper_arm_link',\
+#                     'ra_forearm_link', 'ra_wrist_1_link', 'ra_wrist_2_link', 'ra_wrist_3_link']
 
-    link_names = ['ra_base_link', 'ra_shoulder_link', 'ra_upper_arm_link',\
-                    'ra_forearm_link', 'ra_wrist_1_link', 'ra_wrist_2_link', 'ra_wrist_3_link']
+#     robot = world.robot(0)
+#     flipyz = so3.rotation([1,0,0], math.pi / 2)
+#     for i in range(robot.numLinks()):
+#         if robot.link(i).getName() in link_names:
+#             robot.link(i).geometry().transform(flipyz,[0,0,0])
 
-    robot = world.robot(0)
-    flipyz = so3.rotation([1,0,0], math.pi / 2)
-    for i in range(robot.numLinks()):
-        if robot.link(i).getName() in link_names:
-            robot.link(i).geometry().transform(flipyz,[0,0,0])
-
+    
     vis.run()
+    
+#Motion planning
+#1 - Robot translates chord into key locations
+#2 - Robot moves to location above keys
+#3 - Robot descends to keys
+#4 - Robot lifts hand back up
+#5 - Repeat 1-4 for remaining keys
+
+def strToChords(chords):
+    #Loop through each character in the chord string
+    for letter in chords:
+        #Get the contact locations of the keys
+        locations = chordToKeys(letter)
+        #Skip invalid characters
+        if locations == None:
+            continue
+            
+
+#Returns the positions for the fingers to play the keys
+def chordToKeys(world, chord, white_keys, black_keys):
+    print(white_keys.geometry().numElements())
+    print(black_keys.geometry().numElements())
+    print(chord)
+    locations = []
+    for i in range(white_keys.geometry().numElements()):
+        key = white_keys.geometry().getElement(i)
+        bb = key.getBBTight()
+        print(bb)
+        #For white keys the contact point should be closer to the end
+        #Height
+        z = max(bb[0][2], bb[1][2])
+        #Middle of key
+        x = 3 * (bb[0][0] + bb[1][0]) / 4
+        y = 3 * (bb[0][1] + bb[1][1]) / 4
+        locations.append((x,y,z))
+        break
+        
+    return locations
+    
+#Given the finger positions, play the chord
+#three-note chords are assumed for now
+#obj = ik.objective(robotmodellink,local=lclpt,world=wldpt)
+#Fingers will be a list of the links for the fingers
+def playChord(world, robot, fingers, locations, white_keys, black_keys, num_attempts):
+    #Get the initial robot config
+    init_cf = robot.getConfig()
+    attempt_count = 0
+    #Need to find a way to calculate finger coords
+    solver = ik.IKSolver(robot)
+    for i in range(len(locations)):
+        obj = ik.objective(fingers[i],local=finger_coords[i],world=locations[i])
+        solver.add(obj1)
+    config = solver.solve()
+    while attempt_count < num_attempts and (not config or (config and not not_colliding(world, robot, white_keys, black_keys))):
+        config = solver.solve()
+        if not config:
+            print("valid position not found")
+    
+    final_cf = robot.getConfig()
+    
+    #Construct the motion plan for moving, descending, and ascending (move the collision checking here)
+                                            
+                                   
+        
+    
+#To simplify some possible smalelr issues, collisions between the robot and the keys won't be checked
+def not_colliding(world, robot, white_keys, black_keys):
+    for i in range(robot.numLinks()):
+        for j in range(world.numTerrains()):
+            if robot.link(i).geometry().collides(world.terrain(i).geometry()) and world.terrain(i) != keys and world.terrain(i) != black_keys:
+                return False
+        for j in range(world.numRigidObjects()):
+            if robot.link(i).geometry().collides(world.rigidObject(i).geometry()):
+                return False
+    return True
+    
+    
 
 if __name__=='__main__':
     show_env()
